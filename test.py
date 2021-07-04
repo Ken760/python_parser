32789dc721da0094e4101f8ef813c50a2d36432e
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-07-04 13:27:57 +0300

    update

--- test.py
+++ test.py
@@ -1,28 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+from lxml import *
+from lxml.etree import indent
 
-# url = "https://health-diet.ru/table_calorie/"
-# # url = "http://80.78.245.207:8000/"
+# with open("index.html", encoding='utf-8') as file:
+#     src = file.read()
 #
-# headers = {
-#     "Accept": "*/*",
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 "
-#                   "Safari/537.36"
-# }
-# req = requests.get(url, headers=headers)
-# src = req.text
-# # print(src)
+# soup = BeautifulSoup(src, 'lxml')
+# all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
 #
-# with open("index.html", "w", encoding='utf-8') as file:
-#     file.write(src)
+# all_categories_dict = {}
+# for item in all_products_hrefs:
+#     item_text = item.text
+#     item_href = "https://health-diet.ru" + item.get('href')
+#
+#     all_categories_dict[item_text] = item_href
+#
+# with open("all_categories_dict.json", "w", encoding='utf-8') as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
 
-with open("index.html", encoding='utf-8') as file:
-    src = file.read()
+with open("all_categories_dict.json", encoding='utf-8') as file:
+    all_categories = json.load(file)
 
-soup = BeautifulSoup(src, 'lxml')
-all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+for category_name, category_href in all_categories.items():
 
-for item in all_products_hrefs:
-    item_text = item.text
-    item_href = "https://health-diet.ru" + item.get('href')
-    print(f"{item_text}:{item_href}")
+    rep = [",", " ", "-"]
+    for item in rep:
+        if item
+
